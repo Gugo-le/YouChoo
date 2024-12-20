142d64e039d10457b102e5ce9bd7e2f57cbc674e
Gugo-le <rngustmd0719@naver.com>
2024-12-21 01:04:53 +0900

    모델 저장

--- train/train.py
+++ train/train.py
@@ -11,9 +11,3 @@ okt = Okt()
 tokenized_sentences = [okt.morphs(sentence) for sentence in sentences]
 model = Word2Vec(tokenized_sentences, vector_size=100, window=5, min_count=1, workers=4)
 model.save("word2vec_korean.model")
-
-vector = model.wv['자연어']
-print(vector)
-
-similar_words = model.wv.most_similar('자연어', topn=5)
-print(similar_words)
