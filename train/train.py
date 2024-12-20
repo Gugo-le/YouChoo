19023e5ffccaa47b5b1d4fdc431d815e90478d24
Gugo-le <rngustmd0719@naver.com>
2024-12-21 02:15:44 +0900

    불용어 txt

--- train/train.py
+++ train/train.py
@@ -6,19 +6,28 @@ from gensim.models import Word2Vec
 with open('sentences.json', 'r', encoding='utf-8') as f:
     sentences = json.load(f)
 
+def load_stopwords(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        stopwords = [line.strip() for line in f.readlines()]
+    return stopwords
+
+# 불용어 목록 불러오기
+stopwords = load_stopwords('stopwords-ko.txt')
+
 
 okt = Okt()
 
+# 문장 토큰화 (형태소 분석 및 불용어 제거)
 def preprocess_sentence(sentence):
-    
+   
     tokens = okt.morphs(sentence)  
-    # 불용어 제거 (필요에 따라 stopwords 추가)
-    stopwords = ['이', '그', '저', '것', '들', '은', '는', '을', '를', '이', '가', '으로', '에서', '와', '한']
+   
     tokens = [word for word in tokens if word not in stopwords]
     return tokens
 
-# 각 문장을 형태소 분석하고 불용어를 제거한 후 학습용 데이터 준비
+
 tokenized_sentences = [preprocess_sentence(sentence) for sentence in sentences]
+
 
 model = Word2Vec(tokenized_sentences, vector_size=100, window=5, min_count=1, workers=4)
 
