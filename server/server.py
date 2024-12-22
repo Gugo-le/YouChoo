ae887be312a12366fad818192aeaa20c77b79a17
Gugo-le <rngustmd0719@naver.com>
2024-12-22 22:24:28 +0900

    단어 랜덤 추출

--- server/server.py
+++ server/server.py
@@ -15,3 +15,14 @@ def load_fasttext_model(file_path):
     return model
 
 fasttext_model = load_fasttext_model("cc.ko.300.bin")
+
+def get_random_word_from_file(file_path):
+    try:
+        with open(file_path, "r", encoding = "utf-8") as f:
+            words = f.readlines()
+        random_word = random.choice(words).strip()
+        return random_word
+    except Exception as e:
+        print(f"단어 파일을 읽는 중 오류가 발생했습니다: {e}")
+        return None
+    
